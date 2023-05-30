2f14529df1682d9eb18e53c919f65396416751aa
Alexandre Schmidt <ale.tec.hma@gmail.com>
2023-05-29 22:36:47 -0300

    Create getStaticProps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,21 @@
-function MyApp({ Component, pageProps }){
-  return <Component {...pageProps} />
+import { globalStyles } from "@/styles/global"
+import { AppProps } from "next/app"
+
+import Image from "next/image"
+import logoImg from "../assets/logo.svg"
+
+import { Container, Header } from "../styles/pages/app"
+
+globalStyles() //fica fora para não renderizando toda vez, pq não muda
+
+export default function App({ Component, pageProps }: AppProps){
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
-export default MyApp
